052c9e4c86dd5a84d73d3cf4278408c3fcc010ea
Dell Du <18588220928@163.com>
2021-02-25 12:55:19 +0800

    Update.

--- gimp_image_patch/image_patch.c
+++ gimp_image_patch/image_patch.c
@@ -10,42 +10,90 @@
 
 #define PLUG_IN_PROC "plug-in-gimp_image_patch"
 #define IMAGE_PATCH_REQCODE 0x0104
-#define IMAGE_PATCH_URL "ipc:///tmp/image_patch.ipc"
+// #define IMAGE_PATCH_URL "ipc:///tmp/image_patch.ipc"
+#define IMAGE_PATCH_URL "tcp://127.0.0.1:9104"
 
 static void query(void);
 static void run(const gchar * name,
 				gint nparams, const GimpParam * param, gint * nreturn_vals, GimpParam ** return_vals);
 
-int patch(GimpDrawable * drawable)
+TENSOR *patch_source(int image_id)
 {
-	int socket, ret, rescode;
-	TENSOR *source, *target;
+	int i, j, n_layers;
+	IMAGE *layers[2], *source, *mask;
+	TENSOR *result = NULL;
+
+	n_layers = image_layers(image_id, ARRAY_SIZE(layers), layers);
+
+	if (n_layers < 1) {
+		g_message("Error: Image must at least have 1 layes, general 2, one for source and the other for mask.");
+		goto free_layers;
+	}
+	if (n_layers == 2) {
+		if (layers[0]->height != layers[1]->height || layers[0]->width != layers[1]->width) {
+			g_message("Error: The size of source and mask layer is not same.");
+			goto free_layers;
+		}
+	}
+
+	if (n_layers == 2) {
+		mask = layers[0];
+		source = layers[1];
+		image_foreach(source, i, j) {
+			source->ie[i][j].a = (mask->ie[i][j].a > 128)? 0 : 255;
+		}
+	} else {
+		source = layers[0];
+	}
+
+	result = tensor_from_image(source, 1 /* with alpha */);
+
+free_layers:
+	for (i = 0; i < n_layers; i++)
+		image_destroy(layers[i]);
+
+	return result;
+}
+
+int patch(gint32 image_id)
+{
+	int socket, rescode, ret = RET_ERROR;
+	TENSOR *source, *target = NULL;
 
 	socket = client_open(IMAGE_PATCH_URL);
 	if (socket < 0) {
 		g_message("Error: connect server.");
 		return RET_ERROR;
 	}
+	gimp_progress_init("Patch ...");
 
-	source = tensor_fromgimp(drawable, 0, 0, drawable->width, drawable->height);
+	gimp_progress_update(0.1);
+
+	source = patch_source(image_id);
 	if (! tensor_valid(source)) {
+		g_message("Error: Could not got patch source.");
 		client_close(socket);
 		return RET_ERROR;
 	}
+	gimp_progress_update(0.2);
 
-	ret = request_send(socket, IMAGE_PATCH_REQCODE, source);
+    if (request_send(socket, IMAGE_PATCH_REQCODE, source) == RET_OK) {
+        target = response_recv(socket, &rescode);
+    }
+	gimp_progress_update(0.9);
 
-	if (ret == RET_OK) {
-		target = response_recv(socket, &rescode);
-		if (tensor_valid(target) && rescode == IMAGE_PATCH_REQCODE) {
-			tensor_display(target, "patch");
-			tensor_destroy(target);
-		} else {
-			g_message("Error: Remote service is not valid or timeout.");
-			ret = RET_ERROR;
-		}
-	}	
+	if (tensor_valid(target)) {
+		tensor_display(target, "patch");	// Now source has target information !!!
+		tensor_destroy(target);
+
+		ret = RET_OK;
+	} else {
+		g_message("Error: Remote patch service is not availabe (maybe timeout).");
+	}
+
 	tensor_destroy(source);
+
+	gimp_progress_update(1.0);
 
 	return ret;
 }
@@ -92,9 +140,8 @@ run(const gchar * name, gint nparams, const GimpParam * param, gint * nreturn_va
 	static GimpParam values[1];
 	GimpPDBStatusType status = GIMP_PDB_SUCCESS;
 	// GimpRunMode run_mode;
-	GimpDrawable *drawable;
 
-	/* Setting mandatory output values */
+	/* Setting output values */
 	*nreturn_vals = 1;
 	*return_vals = values;
 	values[0].type = GIMP_PDB_STATUS;
@@ -103,28 +150,10 @@ run(const gchar * name, gint nparams, const GimpParam * param, gint * nreturn_va
 		values[0].data.d_status = GIMP_PDB_CALLING_ERROR;
 		return;
 	}
-
 	values[0].data.d_status = status;
 
-	// run_mode = param[0].data.d_int32;
-	drawable = gimp_drawable_get(param[2].data.d_drawable);
-
-	if (gimp_drawable_is_rgb(drawable->drawable_id) && drawable->bpp == 4) {
-		gimp_progress_init("Patch...");
-
-		GTimer *timer;
-		timer = g_timer_new();
-
-		if (patch(drawable) != RET_OK)
-			status = GIMP_PDB_EXECUTION_ERROR;
-
-		g_print("image patch took %g seconds.\n", g_timer_elapsed(timer, NULL));
-		g_timer_destroy(timer);
-	} else {
-		g_message("Drawable is not RGBA format.");
+	if (patch(param[1].data.d_image) != RET_OK)
 		status = GIMP_PDB_EXECUTION_ERROR;
-	}
-	values[0].data.d_status = status;
 
-	gimp_drawable_detach(drawable);
+	values[0].data.d_status = status;
 }
